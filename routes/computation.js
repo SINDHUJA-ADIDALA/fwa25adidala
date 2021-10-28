1d25e166b2a5cbdc2e900a8c320a0e9788a31896
SINDHUJA-ADIDALA <89418465+SINDHUJA-ADIDALA@users.noreply.github.com>
2021-10-27 20:09:33 -0500

    Update computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -6,7 +6,7 @@ const querystring = require('querystring');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var arg = req.query.value;
-  res.send('Exponential applied to:' + arg + "is" + Math.exp(arg));
+  res.send('Exponential applied to:' + arg + "is " + Math.exp(arg));
 });
 
 module.exports = router;
