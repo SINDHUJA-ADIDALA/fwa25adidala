72c98fe4b76f6d1b06cd5dedda863631bd0f6260
SINDHUJA-ADIDALA <89418465+SINDHUJA-ADIDALA@users.noreply.github.com>
2021-10-27 19:47:55 -0500

    Update mydata.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Sindujha Adidala' });
+  res.render('mydata', { title: 'Sindhuja Adidala' });
  
 });
 
